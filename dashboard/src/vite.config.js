0d7270bd88dd2b84a25a6bfa5a3cd34d20c846cc
Arnab Baruah <baruaharnab87@gmail.com>
2025-04-27 20:07:46 +0530

    1.1

--- dashboard/src/vite.config.js
+++ dashboard/src/vite.config.js
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      input: '/src/main.jsx',  // Ensure this points to your main file correctly
+    },
   },
-})
+});
