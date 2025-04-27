e5ca12260b802261c4491f4fd206cb2777585239
Arnab Baruah <baruaharnab87@gmail.com>
2025-04-27 15:55:48 +0530

    p2p manager updated

--- client/core/p2p_manager.js
+++ client/core/p2p_manager.js
@@ -1,8 +1,41 @@
-// Very basic P2P starter using libp2p (IPFS-lite)
+const { createLibp2p } = require('libp2p')
+const { TCP } = require('@libp2p/tcp')
+const { WebSockets } = require('@libp2p/websockets')
+const { PeerId } = require('@libp2p/peer-id')
+const { Bootstrap } = require('@libp2p/bootstrap')
+const { Mdns } = require('@libp2p/mdns')
 
-async function startP2PNode() {
-    console.log('P2P Node started...');
-    // Later: initialize libp2p, listen to peers
+async function startP2P() {
+  // Generate a new Peer ID
+  const peerId = await PeerId.create()
+
+  // Setup libp2p
+  const node = await createLibp2p({
+    peerId: peerId,
+    transports: [
+      new TCP(),
+      new WebSockets()
+    ],
+    streamMuxers: [
+      // Add stream muxers (if required for your app)
+    ],
+    connectionEncryption: [],
+    peerDiscovery: [
+      new Mdns(), // Local network discovery
+      new Bootstrap({
+        list: [
+          // Replace with your peer bootstrap node addresses if needed
+          '/ip4/127.0.0.1/tcp/4001',
+        ],
+      }),
+    ]
+  })
+
+  // Start the node
+  await node.start()
+
+  console.log(`P2P Node started! Peer ID: ${peerId.toB58String()}`)
+  return node
 }
 
-module.exports = { startP2PNode };
+module.exports = { startP2P }
