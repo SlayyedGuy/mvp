e731670d3b54c7de8352ba730d3026a83b8bfd69
Arnab Baruah <baruaharnab87@gmail.com>
2025-04-27 11:30:22 +0530

    dark mode toggle index updated

--- dashboard/src/pages/index.jsx
+++ dashboard/src/pages/index.jsx
@@ -1,3 +1,17 @@
+import DarkModeToggle from '@/components/DarkModeToggle';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-900 text-black dark:text-white p-6 grid gap-6 grid-cols-1 md:grid-cols-2">
+      <div className="col-span-2 flex justify-end">
+        <DarkModeToggle />
+      </div>
+      {/* rest of your components below */}
+    </div>
+  );
+}
+
+
 import StorageUsageChart from '@/components/StorageUsageChart';
 import TokenBalanceCard from '@/components/TokenBalanceCard';
 import PeerListTable from '@/components/PeerListTable';
@@ -15,3 +29,4 @@ export default function Home() {
     </div>
   );
 }
+
