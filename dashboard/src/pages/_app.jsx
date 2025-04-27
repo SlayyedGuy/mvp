266ded79139072630e0382a8ec83e534f673de77
Arnab Baruah <baruaharnab87@gmail.com>
2025-04-27 12:09:35 +0530

    app.jsx updated

--- dashboard/src/pages/_app.jsx
+++ dashboard/src/pages/_app.jsx
@@ -19,3 +19,14 @@ export default function App({ Component, pageProps }) {
     </WagmiConfig>
   );
 }
+
+import { Toaster } from 'react-hot-toast';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <Component {...pageProps} />
+    </>
+  );
+}
