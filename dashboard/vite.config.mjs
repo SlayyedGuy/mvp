c7e9f27245327bbea42d4bdb8e45fd256153ffa3
Arnab Baruah <baruaharnab87@gmail.com>
2025-04-28 10:19:36 +0530

    debugger

--- dashboard/vite.config.mjs
+++ dashboard/vite.config.mjs
@@ -1,10 +1,3 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  root: './src',  // Make sure Vite is serving from the `src` folder
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx',  // Set the entry file
-    },
-  },
-});
+export default defineConfig({});
